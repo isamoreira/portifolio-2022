88ffcda6c7d2382df06b6fdd7bc2403144c8aa3e
Isabella Moreira <babellamoreira@gmail.com>
2022-10-04 14:27:03 -0300

    Update Router.js

--- meuportifolio/src/Routes/Router.js
+++ meuportifolio/src/Routes/Router.js
@@ -3,12 +3,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "../Components/About/About"
 import Works from "../Components/Works/Works"
 import Home from "../Components/Home/Home"
-import Navbar from "./NavBar";
+import CenterNav from "./CenterNav";
 
 export default function Router() {
     return (
       <BrowserRouter>
-        <Navbar link1="Home" link2="About" link3="Works" />
+        <CenterNav link1="Home" link2="About" link3="Works" />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/About" element={<About />} />
