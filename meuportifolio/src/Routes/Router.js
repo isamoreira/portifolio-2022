6fd77aa1500dd2e52056c72606045112a03439aa
Isabella Moreira <babellamoreira@gmail.com>
2022-10-04 16:04:21 -0300

    Update Router.js

--- meuportifolio/src/Routes/Router.js
+++ meuportifolio/src/Routes/Router.js
@@ -8,11 +8,11 @@ import CenterNav from "./CenterNav";
 export default function Router() {
     return (
       <BrowserRouter>
-        <CenterNav link1="Home" link2="About" link3="Works" />
+       <CenterNav link1="Home" link2="About" link3="Works"/>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Works" element={<Works />} />
+          <Route path="/" element={Home} />
+          <Route path="/About" element={About} />
+          <Route path="/Works" element={Works} />
         </Routes>
       </BrowserRouter>
     );
