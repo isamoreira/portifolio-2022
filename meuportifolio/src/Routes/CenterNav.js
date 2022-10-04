19743767b9b93b0cde57d6e45e1e1de84215e5d9
Isabella Moreira <babellamoreira@gmail.com>
2022-10-04 13:47:50 -0300

    Create CenterNav.js

---
+++ meuportifolio/src/Routes/CenterNav.js
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components";
+import {Link} from "react-router-dom"
+
+const Ul = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-flow: row nowrap;
+  li {
+    padding: 20px 30px;
+    color: #fff;
+
+  }
+  @media (max-width: 768px) {
+    flex-flow: column nowrap;
+    background-color: #2C2C2C;
+    position: fixed;
+    transform: ${({ open }) => (open ? "translateX(0)" : "translateX(100%)")};
+    top: 0;
+    right: 0;
+    height: 100vh;
+    width: 300px;
+    padding-top: 3.5rem;
+    transition: transform 0.3s ease-in-out;
+    li {
+      color: #fff;
+    }
+  }
+`;
+
+const CenterNav = ({ open }, link1, link2, link3) => {
+  return (
+    <Ul open={open}>
+      <li><Link to="/">{link1}</Link></li>
+      <li><Link to="/About">{link2}</Link></li>
+      <li><Link to="/Works">{link3}</Link></li>
+    </Ul>
+  );
+};
+
+export default CenterNav;
