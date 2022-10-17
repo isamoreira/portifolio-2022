43f7eb3f75bbabfeef36e85537e3207a7aed2599
Isabella Moreira <babellamoreira@gmail.com>
2022-10-17 17:07:34 -0300

    Update Works.js

--- meuportifolio/src/Components/Works/Works.js
+++ meuportifolio/src/Components/Works/Works.js
@@ -1,7 +1,61 @@
 import React from "react"
+import Cards from "../Works/Cards"
 
 export default function Works(){
+    const data = [
+        {
+            id:"1",
+            url:"https://projetokiriris.vercel.app/",
+            name: "Projeto Kiriris",
+            descricao:"Nam lacinia fermentum ligula et egestas. Cras scelerisque enim vel velit congue pellentesque. Vestibulum tempus quam eget justo dictum mollis. Curabitur placerat congue pellentesque. Nunc ultrices non nisl sed placerat. Nulla ante odio, gravida in velit et, volutpat suscipit sem. Nam feugiat mauris a pharetra ornare.",
+            img:""
+        },
+        {
+            id:"2",
+            url:"https://projetokiriris.vercel.app/",
+            name: "Projeto Kiriris",
+            descricao:"Nam lacinia fermentum ligula et egestas. Cras scelerisque enim vel velit congue pellentesque. Vestibulum tempus quam eget justo dictum mollis. Curabitur placerat congue pellentesque. Nunc ultrices non nisl sed placerat. Nulla ante odio, gravida in velit et, volutpat suscipit sem. Nam feugiat mauris a pharetra ornare.",
+            img:"C:/Users/babel/OneDrive/Documentos/GitHub/portifolio-2022/meuportifolio/src/Components/Assets/imgkiriris.png"
+        },
+        {
+            id:"3",
+            url:"https://projetokiriris.vercel.app/",
+            name: "Projeto Kiriris",
+            descricao:"Nam lacinia fermentum ligula et egestas. Cras scelerisque enim vel velit congue pellentesque. Vestibulum tempus quam eget justo dictum mollis. Curabitur placerat congue pellentesque. Nunc ultrices non nisl sed placerat. Nulla ante odio, gravida in velit et, volutpat suscipit sem. Nam feugiat mauris a pharetra ornare.",
+            img:"../Assets/imgkiriris.png"
+        },
+        {
+            id:"4",
+            url:"https://projetokiriris.vercel.app/",
+            name: "Projeto Kiriris",
+            descricao:"Nam lacinia fermentum ligula et egestas. Cras scelerisque enim vel velit congue pellentesque. Vestibulum tempus quam eget justo dictum mollis. Curabitur placerat congue pellentesque. Nunc ultrices non nisl sed placerat. Nulla ante odio, gravida in velit et, volutpat suscipit sem. Nam feugiat mauris a pharetra ornare.",
+            img:"../Assets/imgkiriris.png"
+        },
+        {
+            id:"5",
+            url:"https://projetokiriris.vercel.app/",
+            name: "Projeto Kiriris",
+            descricao:"Nam lacinia fermentum ligula et egestas. Cras scelerisque enim vel velit congue pellentesque. Vestibulum tempus quam eget justo dictum mollis. Curabitur placerat congue pellentesque. Nunc ultrices non nisl sed placerat. Nulla ante odio, gravida in velit et, volutpat suscipit sem. Nam feugiat mauris a pharetra ornare.",
+            img:"../Assets/imgkiriris.png"
+        },
+        {
+            id:"6",
+            url:"https://projetokiriris.vercel.app/",
+            name: "Projeto Kiriris",
+            descricao:"Nam lacinia fermentum ligula et egestas. Cras scelerisque enim vel velit congue pellentesque. Vestibulum tempus quam eget justo dictum mollis. Curabitur placerat congue pellentesque. Nunc ultrices non nisl sed placerat. Nulla ante odio, gravida in velit et, volutpat suscipit sem. Nam feugiat mauris a pharetra ornare.",
+            img:"../Assets/imgkiriris.png"
+        }
+    ]
     return(
-        <div></div>
+        <div>
+            <div>
+                <h2>My Works</h2>
+                <div>
+                    {data.map((Card) => (
+                    <Cards key={Card.id} data={Card} />
+              ))}
+                </div>
+            </div>
+        </div>
     )
 }
