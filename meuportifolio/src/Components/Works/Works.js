db160b756589234793d8e2f8e73ac21ca648508a
Isabella Moreira <babellamoreira@gmail.com>
2022-10-18 20:38:44 -0300

    Update Works.js

--- meuportifolio/src/Components/Works/Works.js
+++ meuportifolio/src/Components/Works/Works.js
@@ -1,14 +1,17 @@
 import React from "react"
 import Cards from "../Works/Cards"
+import Kiriris from "../Assets/imgkiriris.png"
 
 export default function Works(){
     const data = [
         {
-            id:"1",
+            
+            id:"1"
+
             url:"https://projetokiriris.vercel.app/",
             name: "Projeto Kiriris",
             descricao:"Nam lacinia fermentum ligula et egestas. Cras scelerisque enim vel velit congue pellentesque. Vestibulum tempus quam eget justo dictum mollis. Curabitur placerat congue pellentesque. Nunc ultrices non nisl sed placerat. Nulla ante odio, gravida in velit et, volutpat suscipit sem. Nam feugiat mauris a pharetra ornare.",
-            img:""
+            img:Kiriris
         },
         {
             id:"2",
