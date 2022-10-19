5954d8b8ec41e99117dbc1d6bd1ee1ada0d1ed77
Isabella Moreira <babellamoreira@gmail.com>
2022-10-19 18:09:22 -0300

    Update Works.js

--- meuportifolio/src/Components/Works/Works.js
+++ meuportifolio/src/Components/Works/Works.js
@@ -1,15 +1,25 @@
 import React from "react"
 import Cards from "../Works/Cards"
 import Kiriris from "../Assets/imgkiriris.png"
+import styled from "styled-components"
+
+const ContainerWorks = styled.div`
+
+`
+const ContainerCards = styled.div`
+width: 100%;
+display: flex;
+flex-wrap: wrap;
+padding: 6rem;
+`
 
 export default function Works(){
     const data = [
         {
             
-            id:"1"
-
+            id:"1",
             url:"https://projetokiriris.vercel.app/",
-            name: "Projeto Kiriris",
+            name: "Projeto Kiriris - Full Stack",
             descricao:"Nam lacinia fermentum ligula et egestas. Cras scelerisque enim vel velit congue pellentesque. Vestibulum tempus quam eget justo dictum mollis. Curabitur placerat congue pellentesque. Nunc ultrices non nisl sed placerat. Nulla ante odio, gravida in velit et, volutpat suscipit sem. Nam feugiat mauris a pharetra ornare.",
             img:Kiriris
         },
@@ -18,47 +28,47 @@ export default function Works(){
             url:"https://projetokiriris.vercel.app/",
             name: "Projeto Kiriris",
             descricao:"Nam lacinia fermentum ligula et egestas. Cras scelerisque enim vel velit congue pellentesque. Vestibulum tempus quam eget justo dictum mollis. Curabitur placerat congue pellentesque. Nunc ultrices non nisl sed placerat. Nulla ante odio, gravida in velit et, volutpat suscipit sem. Nam feugiat mauris a pharetra ornare.",
-            img:"C:/Users/babel/OneDrive/Documentos/GitHub/portifolio-2022/meuportifolio/src/Components/Assets/imgkiriris.png"
+            img:Kiriris
         },
         {
             id:"3",
             url:"https://projetokiriris.vercel.app/",
             name: "Projeto Kiriris",
             descricao:"Nam lacinia fermentum ligula et egestas. Cras scelerisque enim vel velit congue pellentesque. Vestibulum tempus quam eget justo dictum mollis. Curabitur placerat congue pellentesque. Nunc ultrices non nisl sed placerat. Nulla ante odio, gravida in velit et, volutpat suscipit sem. Nam feugiat mauris a pharetra ornare.",
-            img:"../Assets/imgkiriris.png"
+            img:Kiriris
         },
         {
             id:"4",
             url:"https://projetokiriris.vercel.app/",
             name: "Projeto Kiriris",
             descricao:"Nam lacinia fermentum ligula et egestas. Cras scelerisque enim vel velit congue pellentesque. Vestibulum tempus quam eget justo dictum mollis. Curabitur placerat congue pellentesque. Nunc ultrices non nisl sed placerat. Nulla ante odio, gravida in velit et, volutpat suscipit sem. Nam feugiat mauris a pharetra ornare.",
-            img:"../Assets/imgkiriris.png"
+            img:Kiriris
         },
         {
             id:"5",
             url:"https://projetokiriris.vercel.app/",
             name: "Projeto Kiriris",
             descricao:"Nam lacinia fermentum ligula et egestas. Cras scelerisque enim vel velit congue pellentesque. Vestibulum tempus quam eget justo dictum mollis. Curabitur placerat congue pellentesque. Nunc ultrices non nisl sed placerat. Nulla ante odio, gravida in velit et, volutpat suscipit sem. Nam feugiat mauris a pharetra ornare.",
-            img:"../Assets/imgkiriris.png"
+            img:Kiriris
         },
         {
             id:"6",
             url:"https://projetokiriris.vercel.app/",
             name: "Projeto Kiriris",
             descricao:"Nam lacinia fermentum ligula et egestas. Cras scelerisque enim vel velit congue pellentesque. Vestibulum tempus quam eget justo dictum mollis. Curabitur placerat congue pellentesque. Nunc ultrices non nisl sed placerat. Nulla ante odio, gravida in velit et, volutpat suscipit sem. Nam feugiat mauris a pharetra ornare.",
-            img:"../Assets/imgkiriris.png"
+            img:Kiriris
         }
     ]
     return(
         <div>
-            <div>
+            <ContainerWorks>
                 <h2>My Works</h2>
-                <div>
+                <ContainerCards>
                     {data.map((Card) => (
                     <Cards key={Card.id} data={Card} />
               ))}
-                </div>
-            </div>
+                </ContainerCards>
+            </ContainerWorks>
         </div>
     )
 }
