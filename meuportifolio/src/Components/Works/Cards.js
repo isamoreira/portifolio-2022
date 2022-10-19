4524da7387079b2bd34474e510b506ae2eef6a87
Isabella Moreira <babellamoreira@gmail.com>
2022-10-19 18:09:13 -0300

    Update Cards.js

--- meuportifolio/src/Components/Works/Cards.js
+++ meuportifolio/src/Components/Works/Cards.js
@@ -3,20 +3,42 @@ import styled from "styled-components"
 
 const ContainerCards = styled.div`
 color: white;
+width: 20rem;
+padding: 3rem;
+
+
 `
 const Imgprojects =styled.img`
-width: 50%;
+width: 20rem;
+padding: 2rem 0rem;
+`
+
+const ContainerChild = styled.div`
+display: flex;
+align-itens: center;
+justify-content: center;
+flex-direction: column;
+
+`
+const H3 = styled.h3`
+border-bottom: solid 2px yellow;
+
+`
+const Txt = styled.p`
+width: 20rem;
+margin-bottom: 2rem;
+text-align: justify;
 `
 
 export default function Cards(data){
     return(
         <ContainerCards>
-            <div>
-                <h3>{data.data.name}</h3>
-                <Imgprojects src={data.data.img} alt="img"/>
-                <p>{data.data.descricao}</p>
-                <a href={data.data.url}> Site do Projeto</a>
-            </div>
+            <ContainerChild>
+                <H3>{data.data.name}</H3>
+                <a href={data.data.url}> <Imgprojects src={data.data.img} alt="img"/> </a>
+                <Txt>{data.data.descricao}</Txt>
+                
+            </ContainerChild>
         </ContainerCards>
     )
 }
